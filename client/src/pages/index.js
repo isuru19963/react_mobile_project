d0e7630626d2003262b510e586837c59631fc8cc
GHLakshani <54178062+GHLakshani@users.noreply.github.com>
2019-10-18 02:03:00 +0530

    Update index.js
    change to insert loginpage class

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,6 @@ import MobileList from './MobileList'
 import MobileInsert from './MobileInsert'
 import MobileUpdate from './MoviesUpdate'
 import Cart from './Cart'
+import FormPage from './LoginPage'
 
-export { MobileList, MobileInsert, MobileUpdate,Cart }
+export { MobileList, MobileInsert, MobileUpdate,Cart,FormPage}
