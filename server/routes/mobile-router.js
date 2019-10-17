032962796f5e9cbb40191a2d99ebdf8b1ab188b4
GHLakshani <54178062+GHLakshani@users.noreply.github.com>
2019-10-18 02:07:13 +0530

    Update mobile-router.js
    ignore

--- server/routes/mobile-router.js
+++ server/routes/mobile-router.js
@@ -3,7 +3,6 @@ const express = require('express')
 // const MovieCtrl = require('../controllers/movie-ctrl')
 const MobileCtrl = require('../controllers/mobile-ctrl')
 const router = express.Router()
-
 // router.post('/movie', MovieCtrl.createMovie)
 // router.put('/movie/:id', MovieCtrl.updateMovie)
 // router.delete('/movie/:id', MovieCtrl.deleteMovie)
@@ -16,4 +15,5 @@ router.delete('/mobile/:id', MobileCtrl.deleteMobile)
 router.get('/mobile/:id', MobileCtrl.getMobileById)
 router.get('/mobile', MobileCtrl.getMobiles)
 router.post('/mobile', MobileCtrl.createMobileCart)
+
 module.exports = router
