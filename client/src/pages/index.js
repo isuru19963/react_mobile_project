c6ebd4c410d73ae606a7b494d9c88b65c8e9ce3c
GHLakshani <54178062+GHLakshani@users.noreply.github.com>
2019-10-18 04:40:06 +0530

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,5 +3,6 @@ import MobileInsert from './MobileInsert'
 import MobileUpdate from './MoviesUpdate'
 import Cart from './Cart'
 import FormPage from './LoginPage'
+import RegPage from './RegisterPage'
 
-export { MobileList, MobileInsert, MobileUpdate,Cart,FormPage}
+export { MobileList, MobileInsert, MobileUpdate,Cart,FormPage,RegPage}
