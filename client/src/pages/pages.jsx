22f9f12ca52f88fb28181e933d52d5a54d755614
GHLakshani <54178062+GHLakshani@users.noreply.github.com>
2019-10-18 02:06:37 +0530

    Update pages.jsx
    add formapage class

--- client/src/pages/pages.jsx
+++ client/src/pages/pages.jsx
@@ -2,6 +2,7 @@ import MoviesList from './MoviesList'
 import MoviesInsert from './MoviesInsert'
 import MoviesUpdate from './MoviesUpdate'
 import MobileCart from './Cart'
+import FormPage from './LoginPage'
 
 
-export { MoviesList, MoviesInsert, MoviesUpdate,MobileCart }
+export { MoviesList, MoviesInsert, MoviesUpdate,MobileCart,FormPage }
